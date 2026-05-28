c5147227d64025809ea45a6295b70c5eddb3fc8e
nxen <nxen816@gmail.com>
2026-05-29 00:41:28 +0200

    Update bot template

--- resources/bot-template-src/src/interfaces/ActionsCacheOptions.ts
+++ resources/bot-template-src/src/interfaces/ActionsCacheOptions.ts
@@ -1,8 +1,10 @@
-import type { Message, Interaction } from "discord.js";
+import type { Guild, Channel, Message, Interaction } from "discord.js";
 
 export interface ActionsCacheOptions {
   actions: any[];
   index?: number;
+  guild?: Guild;
+  channel: Channel;
   message?: Message;
   interaction?: Interaction;
 }
--- resources/bot-template/src/ActionsCache.js
+++ resources/bot-template/src/ActionsCache.js
@@ -1,11 +1,15 @@
 export class ActionsCache {
     actions;
     index;
+    guild;
+    channel;
     message;
     interaction;
     constructor(options) {
         this.actions = options.actions;
         this.index = options.index || 0;
+        this.guild = options.guild;
+        this.channel = options.channel;
         this.message = options.message;
         this.interaction = options.interaction;
     }
